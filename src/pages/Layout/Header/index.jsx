ddd6ee9453d0f08782d2011bca8d5cbf92aa1fb6
鸡斯拉 <594244274@qq.com>
2023-08-08 22:59:03 +0800

    feat: header完成

--- src/pages/Layout/Header/index.jsx
+++ src/pages/Layout/Header/index.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react';
+import cls from 'classnames';
 import { Link } from 'react-router-dom';
 import './style.less';
 
 const navList = [
-  { title: '九子系列', path: '/detail/:group', img: '' },
-  { title: '情人节系列', path: '/detail/:group', img: '' },
-  { title: '球宝贝系列', path: '/detail/:group', img: '' },
-  { title: '探索者系列', path: '/detail/:group', img: '' },
+  {
+    title: '九子系列',
+    path: '/detail/nine',
+    img: 'public/cls_jiuzi.jpg',
+  },
+  {
+    title: '机车系列',
+    path: '/detail/loco',
+    img: 'public/cls_jiche.jpg',
+  },
+  {
+    title: '情人节系列',
+    path: '/detail/lover',
+    img: 'public/cls_qing.jpg',
+  },
+  {
+    title: '球宝贝系列',
+    path: '/detail/ball',
+    img: 'public/cls_qiu.jpg',
+  },
 ];
 
 const menuList = [
@@ -16,33 +33,60 @@ const menuList = [
 ];
 
 const Header = () => {
-  const [activeNav, setActiveNav] = useState(NaN);
+  const [showNav, setShowNav] = useState(false);
+  const [activeNav, setActiveNav] = useState(0);
 
   return (
     <div class="layout-header">
-      <div class="toggle-btn"></div>
-      <div class="nav-con">
-        <div className="img-blk">{activeNav}</div>
-        <div class="nav-blk">
-          <ul class="nav-list">
-            {navList.map(({ title, path }, idx) => (
-              <li
-                className="nav-item"
-                key={idx}
-                onMouseOver={() => setActiveNav(idx)}
-                onMouseLeave={() => setActiveNav(NaN)}
-              >
-                <Link to={path}>{title}</Link>
-              </li>
-            ))}
-          </ul>
-          <ul class="menu-list">
-            {menuList.map(({ title, path }) => (
-              <li className="menu-item" key={title}>
-                <Link to={path}>{title}</Link>
-              </li>
-            ))}
-          </ul>
+      <div className="default-header">
+        <div class="logo">
+          <img src="public/cls_logo.jpg" />
+        </div>
+        <div class="show-btn" onClick={() => setShowNav(true)}>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={cls('header-con', { active: showNav })}>
+        <div
+          class={cls('close-btn', { active: showNav })}
+          onClick={() => setShowNav(false)}
+        >
+          <span></span>
+          <span></span>
+        </div>
+        <div class="nav-con">
+          <div className="img-blk">
+            {activeNav ? (
+              <img class="cls-img" src={navList[activeNav - 1].img} />
+            ) : (
+              <div class="logo-blk">
+                <img class="img-logo" src="public/cls_logo.jpg" />
+              </div>
+            )}
+          </div>
+          <div class="nav-blk">
+            <ul class="nav-list">
+              <li class="title">产品系列</li>
+              {navList.map(({ title, path }, idx) => (
+                <li
+                  className="nav-item"
+                  key={idx}
+                  onMouseOver={() => setActiveNav(idx + 1)}
+                  onMouseLeave={() => setActiveNav(0)}
+                >
+                  <Link to={path}>{title}</Link>
+                </li>
+              ))}
+            </ul>
+            <ul class="menu-list">
+              {menuList.map(({ title, path }) => (
+                <li className="menu-item" key={title}>
+                  <Link to={path}>{title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
